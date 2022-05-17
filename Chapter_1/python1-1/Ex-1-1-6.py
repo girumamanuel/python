5d6fca4dca40f95380eada243aecf172fe2c1184
girum <girumsisay21@gmail.com>
2022-05-16 20:48:30 -0400

    Modify useargument.py to compose a program usethree.py that takes three names and writes a proper sentence

---
+++ Chapter_1/python1-1/Ex-1-1-6.py
@@ -0,0 +1,10 @@
+import stdio
+import sys
+
+stdio.write('Hi, ')
+stdio.write(sys.argv[1])
+stdio.write(', ')
+stdio.write(sys.argv[2])
+stdio.write(', and ')
+stdio.write(sys.argv[3])
+stdio.writeln('.')
